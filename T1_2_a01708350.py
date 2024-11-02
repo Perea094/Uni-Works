50601bd1c1f0188794751fcf69b8d19b377fba81
Diego <a01708350@tec.mx>
2024-11-02 15:11:38 -0600

    Delete T1_2_a01708350.py

--- T1_2_a01708350.py
+++
@@ -1,7 +0,0 @@
-# Diego Perea León
-# T1_2_a01708350
-
-cm = float(input("Introduzca una longitud en centímetros: \n"))
-inches = cm / 2.54
-foot = inches / 12
-print(f'Equivalencia en: \nPies: {foot:.2f} \nPulgadas: {inches:.2f}')
